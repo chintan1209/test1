083a43f024effaa74421d0a81dbe7e0706fa1e40
Chintan Fruitwala <80763047+chintan1209@users.noreply.github.com>
2022-04-18 11:55:13 +0530

    Delete assets directory

--- assets/js/localization/messages.en-4be0147d2971e8a4e63bde4fd11622af.js
+++
@@ -1,9 +0,0 @@
-MSG = {
-    fieldIsRequired: "This field is required.",
-    phoneNumberIsValid: "Enter a valid phone number.",
-    failedToSendOrder: "An error occurred while submitting your order. Please try again later.",
-    orderConfirmationSuccessful: "Your order has been successfully sent.",
-    orderConfirmationDetails: "The operator will contact you shortly.",
-    orderConfirmationOnlinePaymentNotReadyDetails: "The operator will contact you shortly then a link for payment will be sent.",
-    proceedToCheckout: "Proceed to checkout"
-};
